b3b2b63701f61c48e9854b7e87fa614bb6632149
Sheila Rabbitt <sheilamarierabbitt@gmail.com>
2021-05-06 19:49:32 -0500

    Add comments to Numberlist.py

--- Numberlist.py
+++ Numberlist.py
@@ -1,10 +1,11 @@
-#
+#Creat an empyt list
 numberlist = []
 
+#using range print numbers 0-9
 x = range(0, 10)
 for n in x:
   print(n)
-
+#using a loop append numbers to numberlist
 for n in numberlist:
     numberlist.append(number)
     print(numberlist)
